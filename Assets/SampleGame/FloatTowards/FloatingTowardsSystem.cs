bb45578275b3e24858369d66ce463cb44987492e
Saeed Barari <barari.saeed.am@outlook.com>
2023-07-02 11:23:57 +0330

    hackmd readme changed

--- Assets/SampleGame/FloatTowards/FloatingTowardsSystem.cs
+++ Assets/SampleGame/FloatTowards/FloatingTowardsSystem.cs
@@ -6,23 +6,32 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Extensions;
 using Unity.Transforms;
+using UnityEngine;
 
 namespace SampleGame.FloatTowards {
     [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
     public partial struct FloatingTowardsSystem : ISystem {
-        [BurstCompile] public void OnCreate(ref SystemState state) 
-            => state.RequireForUpdate<FloatTargetAreaTag>();
+        [BurstCompile] public void OnCreate(ref SystemState state) {
+            state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            state.RequireForUpdate<PhysicsWorldSingleton>();
+            state.RequireForUpdate<FloatTargetAreaTag>();
+        }
 
         [BurstCompile] public void OnUpdate(ref SystemState state) {
             var tagEntity = SystemAPI.GetSingletonEntity<FloatTargetAreaTag>();
             var targetArea = SystemAPI.GetComponent<AreaComponentData>( tagEntity );
             var targetAreaTransform = SystemAPI.GetComponent<LocalTransform>( tagEntity );
+
+            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSingleton.CreateCommandBuffer( state.WorldUnmanaged );
             
             // scheduling the job to execute in parallel (multiple threads)
             new FloatingTowardsSystemJob {
                 elapsedTime = SystemAPI.Time.ElapsedTime,
                 targetArea = targetArea,
-                targetAreaTransform = targetAreaTransform
+                targetAreaTransform = targetAreaTransform,
+                physicsWorldSingleton = SystemAPI.GetSingleton<PhysicsWorldSingleton>(),
+                ecb = ecb.AsParallelWriter()
             }.ScheduleParallel();
         }
 
@@ -32,6 +41,8 @@ namespace SampleGame.FloatTowards {
             [ReadOnly] public AreaComponentData targetArea;
             [ReadOnly] public double elapsedTime;
             [ReadOnly] public LocalTransform targetAreaTransform;
+            [ReadOnly] public PhysicsWorldSingleton physicsWorldSingleton;
+            [ReadOnly] public EntityCommandBuffer.ParallelWriter ecb;
             
             // defining our query parameters here as 'ref' or 'in'
             void Execute([ChunkIndexInQuery] int chunkIndex, [EntityIndexInChunk] int entityIndex, ref FloatTowardsComponentData floatTowards, ref PhysicsVelocity physicsVelocity,
@@ -42,6 +53,8 @@ namespace SampleGame.FloatTowards {
                     floatTowards.random = new ( (uint)(elapsedTime * 100 + chunkIndex + entityIndex) );
                 }
 
+                var direction = math.normalize( floatTowards.targetPoint - transform.Position );
+                
                 // new random point
                 if (elapsedTime > floatTowards.nextReTargetTime) {
                     floatTowards.nextReTargetTime = (float)(elapsedTime + floatTowards.reTargetRate);
@@ -49,8 +62,18 @@ namespace SampleGame.FloatTowards {
                     floatTowards.targetPoint = targetAreaTransform.TransformPoint( point );
                 }
 
+                // check forward
+                var input = new RaycastInput {
+                    Start = transform.Position,
+                    End = transform.Position + direction * 2,
+                    Filter = CollisionFilter.Default
+                };
+                if (physicsWorldSingleton.CastRay( input )) {
+                    // move backwards
+                    direction = -direction;
+                }
+                
                 // move towards point
-                var direction = math.normalize( floatTowards.targetPoint - transform.Position );
                 var moveImpulse = direction * floatTowards.speed;
                 physicsVelocity.ApplyLinearImpulse( physicsMass, transform.Scale, moveImpulse );
             }
